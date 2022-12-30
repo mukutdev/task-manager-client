6ea9f6b213de0bfa0fc61962f7146fc661fefcdf
Mukut Chakrabarty <mukutckb1@gmail.com>
2022-12-30 21:00:02 +0600

    loader-added

--- src/App.js
+++ src/App.js
@@ -2,14 +2,26 @@ import { RouterProvider } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
 import './App.css';
 import { routes } from './Routes/Routes/Routes';
+import { useEffect, useState } from 'react';
+import Loader from './Shared/Loader/Loader';
 
 function App() {
-  return (
-    <div className="App bg-gray-100 dark:bg-slate-900 ">
-        <RouterProvider router={routes} />
-        <Toaster/>
-    </div>
-  );
+
+  const [loading , setLoading] = useState(false)
+
+  useEffect(()=>{
+    setLoading(true)
+    setTimeout(()=>{
+        setLoading(false)
+    } , 2000)
+  } , [])
+
+
+  return loading ? <Loader/> :  
+  <div className="App bg-gray-100 dark:bg-slate-900 ">
+    <RouterProvider router={routes} />
+     <Toaster/>
+</div>
 }
 
 export default App;
